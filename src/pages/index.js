4fc7ad5896efed9528f090d12047e92bdcddf57e
Vergil Wong <wangyufan@pawpaw.cn>
2024-01-08 13:23:53 +0800

    Update social card image and homepage description

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Hello from Vergil`}
+      description="Life is a journey, where you can just go and follow your heart.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
